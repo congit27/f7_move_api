d3310365da549f2b91b17026d5efaffc73a32970
congit27 <conbo1102a@gmail.com>
2023-10-02 16:54:41 +0700

    fix axios err

--- src/util/pushNotifications.js
+++ src/util/pushNotifications.js
@@ -2,17 +2,20 @@
 
 const axios = require('axios');
 
-const EXPO_SERVER_URL = 'https://api.expo.dev/v2/push/send'
+const EXPO_SERVER_URL = 'https://api.expo.dev/v2/push/send';
 
 const sendPushNotification = async (token, title, body) => {
     const message = {
         to: token,
         sound: 'default',
         title,
-        body
+        body,
+    };
+
+    try {
+        const response = await axios.post(EXPO_SERVER_URL, message);
+    } catch (error) {
+        console.error('Lỗi Axios:', error.response.data);
     }
-
-    await axios.post(EXPO_SERVER_URL, message)
-}
-
+};
 module.exports = sendPushNotification;
